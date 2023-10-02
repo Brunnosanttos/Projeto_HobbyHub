786985a96b441a5ab32c9f201d15076a0c42a72f
Brunnosanttos <brunoohsanttos@gmail.com>
2023-10-02 00:51:18 -0300

    Projeto_HobbyHub

---
+++ hobbyhub/src/src/Components/menu.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import {Link} from "react-router-dom";
+import "./menu.css";
+
+function Menu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={`sidebar ${menuOpen ? "active" : ""}`}>
+      <button className={`menu_button ${menuOpen ? "active" : "inactive"}`} onClick={toggleMenu}>
+        <div className="lines_btn_menu right"></div>
+        <div className="lines_btn_menu left"></div>
+        <div id="btn_menu_text">MENU</div>
+      </button>
+      {menuOpen && (
+        <ul className='menu_ul'>
+          <li className='Inicio'><Link to="/">Início</Link></li>
+          <li className='Cadastro'><Link to="Cadastro">Cadastro</Link></li>
+          <li className='Contatos'><Link to="Contatos">Contatos</Link></li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Menu;
---
+++ hobbyhub/src/src/pages/Cadastro/Cadastro.js
@@ -0,0 +1,49 @@
+import React from 'react'; 
+import logoprojeto from '../../assets/logoprojeto.png'; 
+import './Cadastro.css'; 
+
+const Cadastro = () => { 
+  return (
+    <div className='main_cadastro'> {/* Inicia um container principal com classe 'Cadcontainer0' */}
+      <div className='left_cadastro'> {/* Inicia um subcontainer com classe 'Cadcontainer1' */}
+        <h1>Bem-vindo!</h1> {/* Título com classe 'Cadcontainer1h1' */}
+        <p>Comece criando um perfil e explore a ampla gama de hobbies e eventos.</p> {/* Parágrafo com classe 'Cadcontainer1p' */}
+        <img src={logoprojeto} alt="Logo"  /> {/* Imagem do logotipo com classe 'Logo' */}
+      </div>
+      <div className='right_cadastro'> {/* Inicia outro subcontainer com classe 'Cadcontainer2' */}
+        <div className='card_cadastro'>
+          <h1>Criar conta</h1> {/* Título com classe 'Cadcontainer2h1' */}
+          <p>Registre-se com seu Email</p> {/* Parágrafo com classe 'Cadcontainer2p' */}
+
+          <label>
+            <input type="email" className='input_cad' placeholder='Digite seu Email' />
+          </label>
+
+          <label>
+            <input type="text" className='input_cad' placeholder='Digite seu nome completo' />
+          </label>
+
+          <label>
+            <input type="text" className='input_cad' placeholder='Nome de usuário' />
+          </label>
+
+          <label>
+            <input type="date" className='input_cad' placeholder='Selecione a Data de Nascimento' />
+          </label>
+
+          <label>
+            <input type="text" className='input_cad' placeholder='Digite seu CPF' />
+          </label>
+
+          <label>
+            <input type="password" className='input_cad' placeholder='Digite sua Senha' />
+          </label>
+
+          <button className='btn_cad'>Cadastre-se</button> {/* Botão de cadastro com classe 'Cadbutton1' */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cadastro; 
